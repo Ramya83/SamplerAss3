86ed9662c80a00541611043681054dbf5e6fefe3
ramya radhakrishnan <ramyaradhakrishnan@ramyas-macbook-pro.local>
2015-10-23 20:13:07 -0700

    Initial Commit

---
+++ SamplerAss3/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SamplerAss3
+//
+//  Created by ramya radhakrishnan on 10/23/15.
+//  Copyright © 2015 ramya radhakrishnan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
